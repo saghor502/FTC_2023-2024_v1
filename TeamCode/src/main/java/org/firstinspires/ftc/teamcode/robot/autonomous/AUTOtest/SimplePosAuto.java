3c2109a6cc19594c7c34ced1e766367c447b73f7
PouTics <alexfusa84@gmail.com>
2023-10-07 18:48:24 -0600

    SimpleOdoFunctions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/AUTOtest/SimplePosAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/AUTOtest/SimplePosAuto.java
@@ -1,23 +1,37 @@
-package org.firstinspires.ftc.teamcode.robot.tlop;
+package org.firstinspires.ftc.teamcode.robot.autonomous.AUTOtest;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.odometry.position.Encoder;
-@TeleOp
-public class newTlop extends LinearOpMode {
+
+import org.firstinspires.ftc.teamcode.Odo.SimplePos;
+import org.firstinspires.ftc.teamcode.odometry.position.Position;
+
+@Autonomous
+public class SimplePosAuto extends LinearOpMode {
     private DcMotor rightFront;
     private DcMotor rightBack;
     private DcMotor leftFront;
     private DcMotor leftBack;
-    private double EncoderX;
+    private double EncoderX; //R
     private double EncoderY;
+    private double Encoderθ; //L
 
     //--------Config
     public static double RWYCoordinate = 10;
-    public static double
-    //--------------
-    @Override
+    public static double LWYCoordinate = 10;
+
+    //--------Map
+
+    private double YdesiredPos = SimplePos.Position(0.50, 2.00);
+    private double XdesiredPos = SimplePos.Position(0.50, 2.00);
+
+    //Constants
+
+    private double turnAngle = SimplePos.getTurn(XdesiredPos, YdesiredPos);
+    private double desiredD = SimplePos.getDistance(XdesiredPos, YdesiredPos);
+
     public void runOpMode(){
         leftFront = hardwareMap.get(DcMotor.class, "lf");
         rightFront = hardwareMap.get(DcMotor.class, "rf");
@@ -25,26 +39,19 @@ public class newTlop extends LinearOpMode {
         leftBack = hardwareMap.get(DcMotor.class, "lb");
         waitForStart();
         while(!isStopRequested()){
+
+            double TurnError = SimplePos.SimpleTurn(EncoderX, Encoderθ, RWYCoordinate, LWYCoordinate, turnAngle);
+
             EncoderX = Encoder.getEncoder(leftFront);
             EncoderY = Encoder.getEncoder(rightBack);
+            Encoderθ = Encoder.getEncoder(leftBack);
             telemetry.addData("X", EncoderX);
             telemetry.addData("Y", EncoderY);
+            telemetry.addData("θErr", TurnError);
             telemetry.update();
 
-            if (gamepad1.right_stick_y < 0.1){
-                forward(gamepad1.right_stick_y);
-            } else if (gamepad1.right_stick_y < 0.1){
-                forward(-gamepad1.right_stick_y);
-            } else {
-                forward(0);
-            }
-            if (gamepad1.left_stick_x < 0.1){
-                turn(gamepad1.left_stick_x);
-            } else if (gamepad1.left_stick_x < 0.1){
-                turn(-gamepad1.left_stick_x);
-            } else {
-                turn(0);
-            }
+
+
         }
     }
     public void forward(double pow){
